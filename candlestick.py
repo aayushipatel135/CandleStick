9a415d91e3c8d6bebcf0eac0b8a710e10f5219a5
Aayushi Patel <126417125+aayushipatel135@users.noreply.github.com>
2023-08-19 16:38:44 +0530

    Update candlestick.py

--- candlestick.py
+++ candlestick.py
@@ -89,7 +89,7 @@ app.layout = html.Div([
               animate=False),
             dcc.Interval(
                 id='graph-update',
-                interval=2000
+                interval=1
             ),
 
 ])
