496437a6ff0587404168be1fd5253625e7dfb9ee
Aayushi Patel <126417125+aayushipatel135@users.noreply.github.com>
2023-08-19 18:10:15 +0530

    Update candlestick.py

--- candlestick.py
+++ candlestick.py
@@ -88,8 +88,8 @@ app.layout = html.Div([
     html.Div(id='toggle-switch-output'),
 
     dcc.Graph(id='live-graph', 
-              # style={'height': '85vh'
-              #       },
+              style={'height': '85vh'
+                    },
               animate=False),
             dcc.Interval(
                 id='graph-update',
