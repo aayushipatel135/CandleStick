be71f1e285250597f7958d784930478207808115
Aayushi Patel <126417125+aayushipatel135@users.noreply.github.com>
2023-08-20 19:27:58 +0530

    Update candlestick.py

--- candlestick.py
+++ candlestick.py
@@ -66,7 +66,7 @@ low.append(df.iloc[0,6])
 close = []
 close.append(df.iloc[0,7])
 display = []
-display.append([df.iloc[0,-1],df.iloc[0,4],df.iloc[0,5],df.iloc[0,6],df.iloc[0,7],df.iloc[0,9]])
+display.append( "Date : "+ str(df.iloc[0,-1]) +  "\n" + "Open : "+ str(df.iloc[0,4]) + "\n" + "high" + str(df.iloc[0,5]) + "\n" + "low" + str(df.iloc[0,6]) + "\n" + "close" + str(df.iloc[0,7]) + "\n" +  "no. of stocks" + str(df.iloc[0,9]) )
 last = 0
 
 
@@ -275,7 +275,7 @@ def update_output(value,data):
                     high.append(df.iloc[last,5])
                     low.append(df.iloc[last,6])
                     close.append(df.iloc[last,7])
-                    display.append([df.iloc[last,-1],df.iloc[last,4],df.iloc[last,5],df.iloc[last,6],df.iloc[last,7],df.iloc[last,9]])
+                    display.append( "Date : "+ str(df.iloc[last,-1]) +  "\n" + "Open : "+ str(df.iloc[last,4]) + "\n" + "high : " + str(df.iloc[last,5]) + "\n" + "low : " + str(df.iloc[last,6]) + "\n" + "close : " + str(df.iloc[last,7]) + "\n" +  "no. of stocks : " + str(df.iloc[last,9])  )
             
                     candle = plotly.graph_objs.Candlestick(x = list(x),
                             low = list(low),
@@ -361,7 +361,7 @@ def update_output(value,data):
                     high.append(df.iloc[last,5])
                     low.append(df.iloc[last,6])
                     close.append(df.iloc[last,7])
-                    display.append([df.iloc[last,-1],df.iloc[last,4],df.iloc[last,5],df.iloc[last,6],df.iloc[last,7],df.iloc[last,9]])
+                    display.append(  "Date : "+ str(df.iloc[last,-1]) +  "\n" + "Open : "+ str(df.iloc[last,4]) + "\n" + "high : " + str(df.iloc[last,5]) + "\n" + "low : " + str(df.iloc[last,6]) + "\n" + "close : " + str(df.iloc[last,7]) + "\n" +  "no. of stocks : " + str(df.iloc[last,9])   )
                     
                     candle = plotly.graph_objs.Candlestick(
                             x = list(x),
